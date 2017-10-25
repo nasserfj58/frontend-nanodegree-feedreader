0cd8ae32989333c8514c4edd69aef049bd8b5dff
nasserfj58 <nasserfj58@gmail.com>
2017-10-25 21:06:01 +0300

    fix issues in the last test and renaming the varibles

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -1,17 +1,12 @@
 
 $(function() {
-      function Feed(name,url){
-        name = this.name;
-        url = this.url;
-      }
-        describe('RSS Feeds', function() {
 
+    describe('RSS Feeds', function(){
 
         it('are defined', function() {
 
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
-
         });
 
          it('Urls defined',function(){
@@ -22,12 +17,8 @@ $(function() {
              expect(item.url).toBeDefined();
              //check if the url is not empty
              expect(item.url).not.toEqual("");
-
            });
-
-
          });
-
 
          it('Names defined',function(){
 
@@ -38,80 +29,78 @@ $(function() {
                 //check if the name is not empty
                 expect(item.name).not.toEqual("");
            });
-
-
          });
-
     });
 
-
-
-    describe('The menu', function() {
-
-
+    describe('The menu', function(){
 
           it('hidden by defualt',function(){
 
-            // check if body have menu-hidden class
+            // check if body have menu-hidden class by defualt
              expect($('body').hasClass("menu-hidden")).toBe(true);
           });
 
-          it('is hidden when click',function(){
-            // if the link that has the menu-icon-link class clicked then the body will not have the menu hidden class
+          it('is showing and hidding when clicked twise',function(){
+            // if the link that has the menu-icon-link class clicked, then the body will not have the menu hidden class
             $('.menu-icon-link').click();
-            // check if body not  have menu-hidden class
+            // check if body not have menu-hidden class after link is clicked
              expect($('body').hasClass("menu-hidden")).not.toBe(true);
 
              // if the link that has the menu-icon-link class clicked clicked then the body will have the menu hidden clas
             $('.menu-icon-link').click();
-          // if the body has the menu hidden class then the menu is hidden by default
+
+          // check if the body has the menu hidden class after the second click on the link
           expect($('body').hasClass("menu-hidden")).toBe(true);
+
         });
     });
 
-      describe('Initial Entries', function() {
+    describe('Initial Entries', function(){
 
-      beforeEach(function(done) {
-        //will whait until async is done
+      beforeEach(function(done){
+        //load the fedd in index 0 and pass the done callback
         loadFeed(0,done);
       });
 
       it('at least one entry',function(){
-           //check if div that has feed class have any link (linls>0)
+           //check if div with class feed has any child with entry class
            expect($('.feed  .entry').length >= 1).toBe(true);
       });
    });
 
-    describe('New Feed Selection', function() {
-          //container before loadFeed();
-          var containerContent,containerContent2;
+    describe('New Feed Selection', function(){
 
-          beforeEach(function(done) {
+        var udacityFeeds;
+        var cssTricksFeeds;
 
-            //will whait until async is done
-              loadFeed(0,function(){
-                 //set to feed div after first call
-              containerContent = $('.feed').html();
+        beforeEach(function(done){
+            // load udacityFeeds (index 0)
+            loadFeed(0,function(){
 
-
+              //set udacity feed
+              udacityFeeds = $('.feed').html();
+              //load cssTricksFeeds (index 1)
+              loadFeed(1,function(){
+                // make sure firs and second calss is done is done
+                done();
               });
-              //will whait until async is done
-              loadFeed(1,function(done){
-                //set to feed div after first call
-                containerContent2 = $('.feed').html();
-
-              });
-
-              done();
-
-
-
+            });
           });
 
-          it('Content is Changing',function(done){
-               //check it the container before loadFeed() has the same elements after load feed
-            expect(containerContent).not.toMatch(containerContent2);
-            done();
+          it('Content is Changing',function(){
+
+            /*
+            To make sure the second loadFeed() call is executed
+            I set the value of cssTricksFeeds inside the it callback function,
+            becuse if I set cssTricksFeeds in beforeEach inside the loadFeed(0,function(){});
+            it will be the same value as the udacityFeeds, and if I set it inside loadFeed(1,function(){})
+            the jasmin time out error will be thrown and test case will fail
+          */
+            cssTricksFeeds = $('.feed').html();
+
+            //check it the html of the first feed will have the same elements as the second feed
+            expect(udacityFeeds).not.toEqual(cssTricksFeeds);
+
          });
    });
 }());
